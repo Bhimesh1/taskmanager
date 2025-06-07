df5ac234657ffa8813a4fe496fae5180ae4f9283
Bhimesh <bhimeshpatil1@gmail.com>
2025-06-07 09:00:25 +0200

    Initial commit: Project structure created

---
+++ main.go
@@ -0,0 +1,72 @@
+// main.go
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+var tasks []Task
+var currentID int = 1
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/tasks", getTasks).Methods("GET")
+	router.HandleFunc("/tasks", createTask).Methods("POST")
+	router.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
+	router.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
+
+	fmt.Println("Server running at http://localhost:8088")
+	http.ListenAndServe(":8088", router)
+}
+
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// If tasks is nil, return an empty slice instead
+	if tasks == nil {
+		tasks = []Task{}
+	}
+
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	json.NewDecoder(r.Body).Decode(&task)
+	task.ID = currentID
+	currentID++
+	tasks = append(tasks, task)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(task)
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+	for i, t := range tasks {
+		if t.ID == id {
+			json.NewDecoder(r.Body).Decode(&tasks[i])
+			tasks[i].ID = id // keep original ID
+			break
+		}
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+	for i, t := range tasks {
+		if t.ID == id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			break
+		}
+	}
+	w.WriteHeader(http.StatusOK)
+}
